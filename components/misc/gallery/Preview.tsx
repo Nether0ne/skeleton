fcaa28192c62868c02e052ff77484ac597d17825
nether0ne <yanstoyanov2012@gmail.com>
2022-10-23 20:52:33 +0300

    feat: basic preview

--- components/misc/gallery/Preview.tsx
+++ components/misc/gallery/Preview.tsx
@@ -2,9 +2,9 @@ import { FC } from "react";
 import Image from "next/image";
 
 interface Props {
-  url: string;
+  img: string;
 }
 
-export const Preview: FC = ({ url }) => {
-  return <></>;
+export const Preview: FC<Props> = ({ img }) => {
+  return <Image src={img} width={"100%"} height={"100%"} alt="Preview" />;
 };
