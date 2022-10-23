5cb0dc01485ea66118211994fbaa1a9f2854e0ac
nether0ne <yanstoyanov2012@gmail.com>
2022-10-23 20:50:10 +0300

    fix: add jpg extension to file types
    fix: reset event target value on image upload

--- components/forms/AddImage/index.tsx
+++ components/forms/AddImage/index.tsx
@@ -3,21 +3,19 @@ import { ChangeEvent, FC, useRef, useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import { useSnackbar } from "notistack";
 
-const fileTypes = ["jpeg", "png", "bmp"].reduce((a, s) => `${a}, image/${s}`);
+const fileTypes = ["jpg", "jpeg", "png", "bmp"].reduce(
+  (a, s) => `${a}, image/${s}`,
+);
 
 interface Props {
   onAdd: (p: string) => void;
 }
 
 export const AddImageForm: FC<Props> = ({ onAdd }) => {
-  const { enqueueSnackbar } = useSnackbar();
-  const [isUploading, setIsUploading] = useState(false);
   const inputFile = useRef<HTMLInputElement>(null);
 
   const onClick = () => {
-    setIsUploading(true);
-    // @ts-ignore
-    inputFile.current.click();
+    inputFile?.current?.click();
   };
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -27,10 +25,9 @@ export const AddImageForm: FC<Props> = ({ onAdd }) => {
       if (newImage) {
         const newPhotoUrl = URL.createObjectURL(newImage);
         onAdd(newPhotoUrl);
-        e.target.files = null;
+        e.target.value = "";
       }
     }
-    setIsUploading(false);
   };
 
   return (
@@ -39,27 +36,20 @@ export const AddImageForm: FC<Props> = ({ onAdd }) => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        width: "100%",
-        height: "100%",
         transition: "background-color .5s ease-out",
-        pointerEvents: !isUploading ? "auto" : "none",
         "&:hover": {
           backgroundColor: "#382E7E",
           transition: "background-color .5s ease-out",
-          cursor: !isUploading ? "pointer" : "wait",
+          cursor: "pointer",
         },
       }}
       onClick={onClick}>
       <ImageListItem key="addPhotoButton">
-        {!isUploading ? (
-          <AddIcon
-            sx={{
-              fontSize: "5rem",
-            }}
-          />
-        ) : (
-          <CircularProgress />
-        )}
+        <AddIcon
+          sx={{
+            fontSize: "5rem",
+          }}
+        />
       </ImageListItem>
       <input
         type="file"
